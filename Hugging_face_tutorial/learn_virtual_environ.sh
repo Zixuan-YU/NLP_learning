3137c3b35e815360bba2f875d8abe70a1961a65d
Avery Yu <cecilia_yu@outlook.com>
2024-09-17 21:35:31 -0700

    updated huggingface trials and .gitignore

---
+++ Hugging_face_tutorial/learn_virtual_environ.sh
@@ -0,0 +1,15 @@
+# create a virtual environment
+# tutorial: https://packaging.python.org/en/latest/guides/installing-using-pip-and-virtual-environments/
+cd /Users/ceciliayu/Documents/NLP_learning_2024
+# create virtual environment
+python3 -m venv .venv
+# activate a virtual environment
+source .venv/bin/activate
+
+which python
+(.venv) ceciliayu@Zixuans-MacBook-Pro NLP_learning_2024
+# since I am using git to track the home dir 'NLP_learning_2024'
+# I will need to add exclude my virtual environment 
+# create the dir named 'venv' in the home dir
+# then add '/venv' to the .gitignore
+
